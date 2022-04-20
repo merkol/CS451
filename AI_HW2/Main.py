b3e720b1661a274e94c0bebe7ef0eff93a82c66c
merkol <merterkol@outlook.com>
2022-04-20 23:14:56 +0300

    Update fitness calc

--- AI_HW2/Main.py
+++ AI_HW2/Main.py
@@ -1,3 +1,4 @@
+from posixpath import split
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,15 +16,29 @@ if __name__ == '__main__':
         Main method
     """
     
-    chromosomes = 16
+    chromosomes = 20
     genes = 12
     generations = 2000
     population = np.random.uniform(-1,1,(chromosomes,genes))
    
-    #splitted = np.hsplit(population,2)
-    for generation in range(generations):
+    #arr = np.zeros(shape=(chromosomes,2))
+    a = []
+    for i in range(chromosomes):
+        splitted = np.hsplit(population[i],2)
+        for j in range(2):
+            if j == 0:
+                splitted[j] = np.reshape(splitted[j],(3,2))
+            else:
+                splitted[j] = np.reshape(splitted[j],(2,3))
+    
+            b = splitted[j]
+            a.append(b)
+        
+    print(len(a))
+    #reshaped = np.reshape(splitted[0],(3,2))
+    """ for generation in range(generations):
         if generation % 200 == 0:
-            print(("Generation:", generation+1))
+            print(("Generation:", generation)) """
     
   
     
