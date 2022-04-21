2fc9a2dd73ed6f9cdc535cad9b2ddc4712351d10
merkol <merterkol@outlook.com>
2022-04-21 16:13:21 +0300

    Fitness Calculation Generation 0 completed

--- AI_HW2/Main.py
+++ AI_HW2/Main.py
@@ -20,7 +20,8 @@ if __name__ == '__main__':
     genes = 12
     generations = 2000
     population = np.random.uniform(-1,1,(chromosomes,genes))
-   
+    population = np.around(population,4)
+    fitness = []
     
     population_reshaped = []
     for i in range(chromosomes):
@@ -38,20 +39,25 @@ if __name__ == '__main__':
     population_reshaped = np.reshape(population_reshaped,(20,2))
     
     predict = []
-    for i in range(len(x_df)):
-        x = x_df.loc[i,['X1','X2','X3']].to_numpy()
-        x = np.reshape(x,(1,3))
-        H = np.dot(x,population_reshaped[0][0])
-        F = np.dot(H,population_reshaped[0][1])
+    for j in range(chromosomes):
+        for i in range(len(x_df)):
+            x = x_df.loc[i,['X1','X2','X3']].to_numpy()
+            x = np.reshape(x,(1,3))
+            H = np.dot(x,population_reshaped[j][0])
+            F = np.dot(H,population_reshaped[j][1])
 
-        y_predict = np.sum(F,axis=-1)
-        predict.append(y_predict[0])
-    x_df['Y_Predicted'] = predict
-   
+            y_predict = np.sum(F,axis=-1)
+            predict.append(abs(y_predict[0]-x_df.loc[i,['Y']][0]))
+    
+        x_df['Fitness'] = predict
+        predict = []
+        fitness.append(x_df['Fitness'].sum())
+        x_df.drop(['Fitness'],inplace=True,axis=1)
+    
     print(x_df.head())
-
-
-    #reshaped = np.reshape(splitted[0],(3,2))
+    fitness = [round(i,3) for i in fitness] 
+    print(fitness)
+  
     """ for generation in range(generations):
         if generation % 200 == 0:
             print(("Generation:", generation)) """
